9604b8b982d56535d92819fd6672a74cd54c1610
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-05-09 15:28:18 +0200

    Added description to UserBean

--- software-engeneering-wildfly-archetype/software-engeneering-wildfly-archetype-ejb/src/main/java/UserBean.java
+++ software-engeneering-wildfly-archetype/software-engeneering-wildfly-archetype-ejb/src/main/java/UserBean.java
@@ -6,12 +6,17 @@ import java.io.Serializable;
 public class UserBean implements Serializable {
     protected String userName;
     protected String password;
+    protected String description;
 
     public String getUserName() {
         return userName;
     }
     public void setUserName(String newUserName) {
         userName = newUserName;
+    }
+    public String getDescription() {return description;}
+    public void setDescription(String newDescription) {
+        description = newDescription;
     }
     public String getPassword(String password) {
         return password;
