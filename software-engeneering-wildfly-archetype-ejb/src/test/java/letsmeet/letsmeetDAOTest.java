0b08ded0008835507deef2654e27de126439368a
Napfkuchen <user@user-virtual-machine>
2016-05-25 00:23:57 +0200

    Data Access Object Test-Umgebung angelegt

---
+++ software-engeneering-wildfly-archetype-ejb/src/test/java/letsmeet/letsmeetDAOTest.java
@@ -0,0 +1,15 @@
+package letsmeet;
+
+import javax.ejb.EJB;
+
+import org.jboss.arquillian.container.test.api.Deployment; 
+import org.jboss.arquillian.junit.Arquillian; 
+import org.jboss.shrinkwrap.api.ShrinkWrap; 
+import org.jboss.shrinkwrap.api.spec.WebArchive; 
+import org.junit.Test; 
+import org.junit.runner.RunWith;
+
+@RunWith (Aquillian.class)
+public class letsmeetDAOTest {
+
+}
