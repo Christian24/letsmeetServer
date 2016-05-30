0474abec8bc3ba91515cb0b16984c35a84f407cb
Christian Siebmanns <christian.siebmanns@hotmail.de>
2016-05-30 18:06:36 +0200

    DataBuilder

---
+++ software-engeneering-wildfly-archetype-ejb/src/main/java/dataAccess/DataBuilder.java
@@ -0,0 +1,40 @@
+package dataAccess;
+
+import meet.Category;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+/**
+ * Created by Christian on 30.05.2016.
+ */
+@Singleton
+@Startup
+public class DataBuilder {
+    protected final String[] categories = {"Sport", "Kultur", "Essen & Trinken", "Feiern", "Kennenlernen"};
+    @EJB
+    protected DataAccessObject dataAccessObject = new EntityManagerDAO();
+    @PostConstruct
+    private void createTestData() {
+    createCategories();
+}
+    /**
+     * Creates our categories
+     */
+    private void createCategories() {
+        for(String category : categories) {
+            createCategory(category);
+        }
+    }
+    private void createCategory(String name) {
+        Category category = dataAccessObject.findCategoryById(name);
+        if(category != null) {
+            Category newCategory = new Category();
+            category.setTitle(name);
+            dataAccessObject.persist(newCategory);
+        }
+    }
+
+}
